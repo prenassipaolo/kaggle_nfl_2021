b6cfa52ca8ca890566572d6ea5b3b905868b599c
prenassipaolo <prenassipaolo@gmail.com>
2021-10-21 19:29:34 +0200

    Add football field plot funcitons

--- utils/display.py
+++ utils/display.py
@@ -1,6 +1,8 @@
 import os
 import cv2
 import pandas as pd
+import matplotlib.patches as patches
+import matplotlib.pylab as plt
 
 def video_with_baseline_boxes(
     video_path: str, baseline_boxes: pd.DataFrame, gt_labels: pd.DataFrame, verbose=True
@@ -154,3 +156,157 @@ def display_video(path):
     cv2.destroyAllWindows()
     return
 
+def create_football_field(
+    linenumbers=True,
+    endzones=True,
+    highlight_line=False,
+    highlight_line_number=50,
+    highlighted_name="Line of Scrimmage",
+    fifty_is_los=False,
+    figsize=(12, 6.33),
+    field_color="lightgreen",
+    ez_color='forestgreen',
+    ax=None,
+):
+    """
+    Function that plots the football field for viewing plays.
+    Allows for showing or hiding endzones.
+    """
+    rect = patches.Rectangle(
+        (0, 0),
+        120,
+        53.3,
+        linewidth=0.1,
+        edgecolor="r",
+        facecolor=field_color,
+        zorder=0,
+    )
+
+    if ax is None:
+        fig, ax = plt.subplots(1, figsize=figsize)
+    ax.add_patch(rect)
+
+    plt.plot([10, 10, 10, 20, 20, 30, 30, 40, 40, 50, 50, 60, 60, 70, 70, 80,
+              80, 90, 90, 100, 100, 110, 110, 120, 0, 0, 120, 120],
+             [0, 0, 53.3, 53.3, 0, 0, 53.3, 53.3, 0, 0, 53.3, 53.3, 0, 0, 53.3,
+              53.3, 0, 0, 53.3, 53.3, 0, 0, 53.3, 53.3, 53.3, 0, 0, 53.3],
+             color='black')
+
+    if fifty_is_los:
+        ax.plot([60, 60], [0, 53.3], color="gold")
+        ax.text(62, 50, "<- Player Yardline at Snap", color="gold")
+    # Endzones
+    if endzones:
+        ez1 = patches.Rectangle(
+            (0, 0),
+            10,
+            53.3,
+            linewidth=0.1,
+            edgecolor="black",
+            facecolor=ez_color,
+            alpha=0.6,
+            zorder=0,
+        )
+        ez2 = patches.Rectangle(
+            (110, 0),
+            120,
+            53.3,
+            linewidth=0.1,
+            edgecolor="black",
+            facecolor=ez_color,
+            alpha=0.6,
+            zorder=0,
+        )
+        ax.add_patch(ez1)
+        ax.add_patch(ez2)
+    ax.axis("off")
+    if linenumbers:
+        for x in range(20, 110, 10):
+            numb = x
+            if x > 50:
+                numb = 120 - x
+            ax.text(
+                x,
+                5,
+                str(numb - 10),
+                horizontalalignment="center",
+                fontsize=20,  # fontname='Arial',
+                color="black",
+            )
+            ax.text(
+                x - 0.95,
+                53.3 - 5,
+                str(numb - 10),
+                horizontalalignment="center",
+                fontsize=20,  # fontname='Arial',
+                color="black",
+                rotation=180,
+            )
+    if endzones:
+        hash_range = range(11, 110)
+    else:
+        hash_range = range(1, 120)
+
+    for x in hash_range:
+        ax.plot([x, x], [0.4, 0.7], color="black")
+        ax.plot([x, x], [53.0, 52.5], color="black")
+        ax.plot([x, x], [22.91, 23.57], color="black")
+        ax.plot([x, x], [29.73, 30.39], color="black")
+
+    if highlight_line:
+        hl = highlight_line_number + 10
+        ax.plot([hl, hl], [0, 53.3], color="yellow")
+        ax.text(hl + 2, 50, "<- {}".format(highlighted_name), color="yellow")
+
+    border = patches.Rectangle(
+        (-5, -5),
+        120 + 10,
+        53.3 + 10,
+        linewidth=0.1,
+        edgecolor="orange",
+        facecolor="white",
+        alpha=0,
+        zorder=0,
+    )
+    ax.add_patch(border)
+    ax.set_xlim((0, 120))
+    ax.set_ylim((0, 53.3))
+    return ax
+
+
+
+def plot_football_field_action(tracks, game_play, event=None):
+    """
+    Plots on the football field the player position of a specific game play
+    during the entire action or at a specific event (the first time it occurs).
+    
+    Returns a dataframe with the filtered data.
+    """
+    # check the presence of the specified game_play
+    possible_game_play = tracks.game_play.unique()
+    if game_play not in possible_game_play:
+        print('Error: The specified game_play is incorrect or absent.')
+        print(f'Allowed game_play: {possible_game_play}')
+        return 
+    # filter by game_play
+    example_tracks = tracks.query(f"game_play == '{game_play}'")
+    if event:
+        # check the presence of the specified event
+        possible_event = example_tracks.event.unique()
+        if event not in possible_event:
+            print('Error: The specified event is incorrect or absent in the specified game_play.')
+            print(f'Allowed event: {possible_event}')
+            return 
+        # filter by event and select the first occurrence
+        example_tracks = example_tracks.query(f"event == '{event}'")
+        # select the first
+        first_event_frame = example_tracks.est_frame.unique().min()
+        example_tracks = example_tracks.query(f"est_frame == {first_event_frame}")
+    # plot the game_play (event)
+    ax = create_football_field()
+    for team, d in example_tracks.groupby("team"):
+        ax.scatter(d["x"], d["y"], label=team, s=65, lw=1, edgecolors="black", zorder=5)
+    ax.legend().remove()
+    ax.set_title(f"Tracking data for {game_play}: at snap", fontsize=15)
+    plt.show()
+    return example_tracks
