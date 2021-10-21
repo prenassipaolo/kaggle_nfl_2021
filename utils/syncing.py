dbcb71939e75d4e67be8377146f6d26615257790
prenassipaolo <prenassipaolo@gmail.com>
2021-10-21 17:20:26 +0200

    add syncing of the 2d data to the videos

---
+++ utils/syncing.py
@@ -0,0 +1,36 @@
+import pandas as pd
+
+def add_track_features(tracks, fps=59.94, snap_frame=10):
+    """
+    Add column features helpful for syncing with video data.
+    """
+    tracks = tracks.copy()
+    tracks["game_play"] = (
+        tracks["gameKey"].astype("str")
+        + "_"
+        + tracks["playID"].astype("str").str.zfill(6)
+    )
+    # convert the time type from string to datetime
+    tracks["time"] = pd.to_datetime(tracks["time"])
+    # identify ball snaps to syncing the videos according to the data descrtiption
+    snap_dict = (
+        tracks.query('event == "ball_snap"')
+        .groupby("game_play")["time"]
+        .first()
+        .to_dict()
+    )
+    # associate the snap time to each play
+    tracks["snap"] = tracks["game_play"].map(snap_dict)
+    # flag each frame as snap or not
+    tracks["isSnap"] = tracks["snap"] == tracks["time"]
+    tracks["team"] = tracks["player"].str[0].replace("H", "Home").replace("V", "Away")
+    # calculate in milliseconds the difference 
+    # between the snap time and the actual frame time and divide by 1000
+    tracks["snap_offset"] = (tracks["time"] - tracks["snap"]).astype(
+        "timedelta64[ms]"
+    ) / 1_000
+    # calculate the estimated video frame unit according to the video
+    tracks["est_frame"] = (
+        ((tracks["snap_offset"] * fps) + snap_frame).round().astype("int")
+    )
+    return tracks
